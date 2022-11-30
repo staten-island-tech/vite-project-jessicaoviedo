8667bc43037a6ba8208b175ad8af70ff83d9c550
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-11-30 10:12:24 -0500

    ahjasvcfjsg dv

---
+++ vite-project/js/main.js
@@ -0,0 +1,7 @@
+import "../styles/style.css";
+// import javascriptLogo from "./javascript.svg";
+
+document.querySelector("#app").innerHTML = `
+  <h1>hello vite!!!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">documentation</a>
+`;
