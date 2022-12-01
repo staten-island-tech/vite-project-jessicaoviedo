b4c5c0b898f47a092c57746f9f445e28c2170866
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-12-01 09:52:22 -0500

    webiovbos

---
+++ vite-project/js/menu.js
@@ -0,0 +1,18 @@
+const plushies = [
+  {
+    name: "hello kitty",
+    price: 10.0,
+    girl: true,
+    boy: false,
+    img: waittttttt,
+    inStock: true,
+  },
+  {
+    name: "kuromi",
+    price: 12.0,
+    girl: true,
+    boy: false,
+    img: waittttttt,
+    inStock: true,
+  },
+];
