d9f24c1f72073c8487ac35f3cf9aa7e0f2ec1f91
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-12-02 10:12:46 -0500

    hfvjasdvhfahsvd

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,10 @@
 import "../styles/style.css";
 // import javascriptLogo from "./javascript.svg";
+import { plushies } from "./menu";
 
 document.querySelector("#app").innerHTML = `
   <h1>hello vite!!!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">documentation</a>
 `;
+
+console.log(plushies);
