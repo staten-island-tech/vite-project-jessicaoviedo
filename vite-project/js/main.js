d1de358dd79618d37fdf8866e502185cda5216e9
jessicaoviedo <113190722+jessicaoviedo@users.noreply.github.com>
2022-12-07 10:14:11 -0500

    sjfvgakvgjksdc

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -11,3 +11,13 @@ document.querySelector("#app").innerHTML = `
 `;
 
 console.log(plushies);
+
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
